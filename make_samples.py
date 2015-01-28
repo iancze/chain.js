4c0a524afb947c1ae2b0205b083bfc003f4a4043
Ian Czekala <iancze@gmail.com>
2015-01-27 21:56:28 -0500

    5 param

--- make_samples.py
+++ make_samples.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-params = ["a", "b", "c", "d"]
+params = ["a", "b", "c", "d", "BEER"]
 nparam = len(params)
 niter = 400
 
@@ -12,9 +12,11 @@ f.close()
 
 for i in range(niter):
 
-    # Insert these three lines of code inside your likelihood call
+    # Insert these lines of code inside your likelihood call
     f = open("mc.csv", "a")
-    f.write(",".join([str(i) for i in np.random.rand(nparam)]) + "\n")
+    proposed_param = np.random.rand(nparam)
+    f.write(",".join([str(i) for i in proposed_param]) + "\n")
     f.close()
 
+# when you're done, be sure to close the file
 f.close()
